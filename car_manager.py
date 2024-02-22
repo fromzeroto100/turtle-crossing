3a4c2b502fc5a4d49d2aeddccd7d67c2fe83b2d5
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-22 20:05:54 +0100

    new5

--- car_manager.py
+++ car_manager.py
@@ -13,13 +13,15 @@ class CarManager(Turtle):
 
 
     def create_cars(self):
-        new_car = Turtle("square")
-        new_car.shapesize(stretch_wid=1, stretch_len=2)
-        new_car.penup()
-        new_car.color(random.choice(COLORS))
-        random_y = random.randint(-250, 250)
-        new_car.goto(300, random_y)
-        self.all_cars.append(new_car)
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
 
 
 
